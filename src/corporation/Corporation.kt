4f00cdc4fde0ff8b833fe4d8d2ec62587662d937
Nikita Ponomarenko <nponomarenko13@mail.ru>
2026-02-15 20:06:16 +0300

    3.11 Upcast, Downcast, Smartcast

--- src/corporation/Corporation.kt
+++ src/corporation/Corporation.kt
@@ -1,9 +1,11 @@
 package corporation
 
 fun main() {
-    val director = Director(name = "Andrey", age = 25)
-    val consultant = Consultant(name = "Max")
-    val assistant = Assistant("Helen", 20)
-    director.getConsultantToWork(consultant)
+    val director: Worker = Director(name = "Andrey", age = 25)
+    val consultant: Worker = Consultant(name = "Max")
+    val assistant: Worker = Assistant("Helen", 20)
+    (assistant as Assistant).bringCoffee()
+    (director as Director).getConsultantToWork(consultant as Consultant)
     director.takeCoffee(assistant)
+    println(director.name)
 }
